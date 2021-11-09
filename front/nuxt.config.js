491d4a5799cec6272e89771517406ee5f2fe06be
JOHEEGUN <whgmlrjs123@naver.com>
2021-11-09 18:14:42 +0900

    meta 태그적용

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -1,6 +1,27 @@
 module.exports = {
   head: {
-      title: 'Vue-SNS',
+    title: 'Vue-SNS',
+    meta: [{
+      charset: 'utf-8',
+    }, {
+      name: 'viewport',
+      content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover',
+    }, {
+      'http-equiv': 'X-UA-Compatible', content: 'IE=edge',
+    }, {
+      hid: 'desc', name: 'description', content: 'Vue-SNS',
+    }, {
+      hid: 'ogtitle', name: 'og:title', content: 'Vue-SNS',
+    }, {
+      hid: 'ogdesc', name: 'og:description', content: 'Vue - Movie-SNS',
+    }, {
+      hid: 'ogtype', property: 'og:type', content: 'website',
+    }, {
+      hid: 'ogimage', property: 'og:image', content: 'https://vue.nodebird.com/vue-nodebird.png',
+    }, {
+      hid: 'ogurl', property: 'og:url', content: 'https://vue.mas.com',
+    }],
+    link: [{ rel: 'shortcut icon', href: '/favicon.ico' }],
   },
   modules: [
     '@nuxtjs/axios',
@@ -11,8 +32,14 @@ module.exports = {
   moment: {
     locales: ['ko'],
   },
-  build:{
-    analyze:true,
+  build: {
+    analyze: false,
+    extend(config, { isClient, isServer, isDev }) {
+      if (isServer && !isDev) {
+        config.devtool = 'hidden-source-map';
+      }
+      console.log('webpack', config, isServer, isClient);
+    },
   },
   vuetify: {},
   axios:{
